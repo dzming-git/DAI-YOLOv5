a54f4cf9ed9448bbaf5f601ba9e97025e00b7b30
dzming <dzm_work@163.com>
2023-10-20 07:25:30 +0000

    v0.16 改用grpc服务端

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 from yolov5_src import YOLOV5Impl
-from service import YOLOv5Service
-
-def connect_consul():
-    pass
+from grpcs.task_coordinate.task_coordinate_server import task_coordinate_serve
+from utils.task_ctrl import TaskCtrl
 
 if __name__ == '__main__':
     yolov5_builder = YOLOV5Impl.YOLOV5Builder()
     yolov5_builder._device = 'cuda:0'
     yolov5_impl = yolov5_builder.build()
-    
-    yolov5_service_builder = YOLOv5Service.SingletonBuilder()
-    yolov5_service_builder.set_host('0.0.0.0')
-    yolov5_service_builder.build()
-    yolov5_service = yolov5_service_builder.get_instance()
-    yolov5_service.set_yolov5_impl(yolov5_impl)
-    yolov5_service.start_server()
+    yolov5_impl.load_model()
+
+    task_ctrl = TaskCtrl()
+    task_ctrl.set_yolov5_impl(yolov5_impl)
+    task_ctrl.listening()
+    task_coordinate_serve('0.0.0.0', '5000', 10)
