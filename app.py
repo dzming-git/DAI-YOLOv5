e705a7927e30bc4532be967262731db9b8f6554b
dzming <dzm_work@163.com>
2023-10-20 08:12:37 +0000

    v0.17 实现向consul注册服务，不再使用python-consul

--- app.py
+++ app.py
@@ -2,7 +2,24 @@ from yolov5_src import YOLOV5Impl
 from grpcs.task_coordinate.task_coordinate_server import task_coordinate_serve
 from utils.task_ctrl import TaskCtrl
 
+def connect_consul():
+    from common.consul_client import ConsulClient
+    from common.utils import get_local_ip_address
+    host = get_local_ip_address()
+    port = 5000
+    service_name = 'target detection'
+    consul_client = ConsulClient()
+    consul_client\
+        .set_service_address(host)\
+        .set_service_port(port)\
+        .set_service_id(f'{service_name}-{host}-{port}')\
+        .set_service_name(service_name)\
+        .set_service_tags(['yolov5', 'grpc'])\
+        .register_service()
+
 if __name__ == '__main__':
+    connect_consul()
+    
     yolov5_builder = YOLOV5Impl.YOLOV5Builder()
     yolov5_builder._device = 'cuda:0'
     yolov5_impl = yolov5_builder.build()
