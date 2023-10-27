4a1de6524f5894caf0b0aa8f66efbb0d66659376
dzming <dzm_work@163.com>
2023-10-27 06:18:10 +0000

    向consul注册时，每个权重文件单独注册一个服务，且将label写入tags

--- app.py
+++ app.py
@@ -13,18 +13,21 @@ def connect_consul():
     consul_client = ConsulClient()
     consul_client.consul_ip = config.consul_ip
     consul_client.consul_port = config.consul_port
-
-    service_info = ServiceInfo()
     host = get_local_ip_address()
     service_name = config.service_name
     service_port = config.service_port
+    service_info = ServiceInfo()
     service_info.service_ip = host
     service_info.service_port = service_port
     service_info.service_name = service_name
-    service_info.service_id = f'{service_name}-{host}-{service_port}'
-    service_info.service_tags = config.service_tags
 
-    consul_client.register_service(service_info)
+    for weight in config.weights:
+        service_info.service_id = f'{service_name}-{weight.file}-{host}-{service_port}'
+        service_info.service_tags = config.service_tags
+        for label in weight.labels:
+            service_info.service_tags.append(f'label:{label}')
+
+        consul_client.register_service(service_info)
 
 if __name__ == '__main__':
     connect_consul()
