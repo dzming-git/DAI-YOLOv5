36c47d38d163f1bab396ded662a4d1358e5d0a55
dzming <dzm_work@163.com>
2023-10-30 03:57:14 +0000

    proto重命名修改遗漏部分 img trans -> image harmony

--- src/grpc/clients/image_harmony/image_harmony_client.py
+++ src/grpc/clients/image_harmony/image_harmony_client.py
@@ -1,4 +1,4 @@
-from generated.protos.img_trans import img_trans_pb2, img_trans_pb2_grpc
+from generated.protos.image_harmony import image_harmony_pb2, image_harmony_pb2_grpc
 import grpc
 import cv2
 from typing import Dict, Tuple
@@ -9,33 +9,33 @@ class ImageHarmonyClient:
     def __init__(self, ip:str, port: str):
         options = [('grpc.max_receive_message_length', 1024 * 1024 * 1024)]
         self.conn = grpc.insecure_channel(f'{ip}:{port}', options=options)
-        self.client = img_trans_pb2_grpc.CommunicateStub(channel=self.conn)
+        self.client = image_harmony_pb2_grpc.CommunicateStub(channel=self.conn)
         self.connect_id: int = -1
     
     def __del__(self):
         if -1 != self.connect_id:
-            unregister_img_trans_service_request = img_trans_pb2.UnregisterImgTransServiceRequest()
-            unregister_img_trans_service_request.connectId = self.connect_id
-            unregister_img_trans_service_response = client.unregisterImgTransService(unregister_img_trans_service_request)
+            unregister_image_harmony_service_request = image_harmony_pb2.UnregisterImgTransServiceRequest()
+            unregister_image_harmony_service_request.connectId = self.connect_id
+            unregister_image_harmony_service_response = client.unregisterImgTransService(unregister_image_harmony_service_request)
             
-            response = unregister_img_trans_service_response.response
+            response = unregister_image_harmony_service_response.response
             print(f'{response.code}: {response.message}')
 
     
     def set_args(self, img_type: str, args: Dict[str, str]):
-        register_img_trans_service_request = img_trans_pb2.RegisterImgTransServiceRequest()
-        register_img_trans_service_request.imgType = img_type
+        register_image_harmony_service_request = image_harmony_pb2.RegisterImgTransServiceRequest()
+        register_image_harmony_service_request.imgType = img_type
         for key, value in args.items():
-            arg = register_img_trans_service_request.args.add()
+            arg = register_image_harmony_service_request.args.add()
             arg.key = key
             arg.value = value
-        register_img_trans_service_response = self.client.registerImgTransService(register_img_trans_service_request)
-        self.connect_id = register_img_trans_service_response.connectId
-        response = register_img_trans_service_response.response
+        register_image_harmony_service_response = self.client.registerImgTransService(register_image_harmony_service_request)
+        self.connect_id = register_image_harmony_service_response.connectId
+        response = register_image_harmony_service_response.response
         print(f'{response.code}: {response.message}')
     
     def get_img(self) -> Tuple[int, cv2.Mat]:
-        get_img_request = img_trans_pb2.GetImgRequest()
+        get_img_request = image_harmony_pb2.GetImgRequest()
         get_img_request.connectId = self.connect_id
         # TODO: 这些参数暂时固定
         get_img_request.format = '.jpg'
