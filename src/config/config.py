78014dfae04008ff8d82f41f6c93bf6e56dfb1d8
dzming <dzm_work@163.com>
2023-11-30 07:39:35 +0000

    修改了config中权重文件信息的存储方式，并添加yolov5官方全部权重信息

--- src/config/config.py
+++ src/config/config.py
@@ -1,6 +1,7 @@
 from src.utils import singleton
 import yaml
 from typing import List, Dict
+import os
 
 CONFIG_PATH = './.config.yml'
 
@@ -31,12 +32,23 @@ class Config:
         self.service_name = service_data.get('name', '')
         self.service_port = str(service_data.get('port', ''))
         self.service_tags = service_data.get('tags', [])
-        weights = service_data.get('weights', [])
-        for weight in weights:
-            weight_info = WeightInfo()
-            weight_info.file = weight['file']
-            weight_info.labels = weight['labels']
-            self.weights_map[weight_info.file] = weight_info
+        weight_info_folder = service_data.get('weight_info_folder', '')
+        weight_info_paths = []
+        
+        if weight_info_folder:
+            for root, dirs, files in os.walk(weight_info_folder):
+                file_name: str
+                for file_name in files:
+                    if file_name.endswith('.yml') or file_name.endswith('yaml'):
+                        file_path = os.path.join(root, file_name)
+                        weight_info_paths.append(file_path)
+        for weight_info_path in weight_info_paths:
+            with open(weight_info_path, 'r') as f:
+                weight_info_data = yaml.safe_load(f)
+                weight_info = WeightInfo()
+                weight_info.file = weight_info_data['file']
+                weight_info.labels = weight_info_data['labels']
+                self.weights_map[weight_info.file] = weight_info
         
         consul_data = config_data.get('consul', {})
         self.consul_ip = consul_data.get('ip', '')
