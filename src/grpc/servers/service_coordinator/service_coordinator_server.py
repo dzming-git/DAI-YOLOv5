15cd75e7580d802c3e7397b363c7d7d103525376
dzming <dzm_work@163.com>
2024-01-24 07:41:35 +0000

    service_coordinator的start方法开始加载权重

--- src/grpc/servers/service_coordinator/service_coordinator_server.py
+++ src/grpc/servers/service_coordinator/service_coordinator_server.py
@@ -5,11 +5,14 @@ import traceback
 from generated.protos.service_coordinator import service_coordinator_pb2, service_coordinator_pb2_grpc
 from typing import Dict
 from src.task_manager.task_manager import TaskManager, TaskInfo
+from src.model_manager.model_manager import ModelManager
+from src.model_manager import model_manager as mm
 
 VALID_PRE_SERVICE = ['image harmony']
 
 class ServiceCoordinatorServer(service_coordinator_pb2_grpc.CommunicateServicer):
     def informPreviousServiceInfo(self, request, context):
+        print(request)
         response_code = 200
         response_message = ''
         
@@ -50,6 +53,7 @@ class ServiceCoordinatorServer(service_coordinator_pb2_grpc.CommunicateServicer)
         return response
     
     def informCurrentServiceInfo(self, request, context):
+        print(request)
         response_code = 200
         response_message = ''
         # TODO: 目前只支持weight、ConnectID
@@ -101,6 +105,41 @@ class ServiceCoordinatorServer(service_coordinator_pb2_grpc.CommunicateServicer)
             response_message += traceback.format_exc()
 
         response = service_coordinator_pb2.InformPreviousServiceInfoResponse()
+        response.response.code = response_code
+        response.response.message = response_message
+        return response
+
+    def start(self, request, context):
+        response_code = 200
+        response_message = ''
+        try:
+            task_manager = TaskManager()
+            model_manager = ModelManager()
+            task_id = request.taskId
+            assert task_id in task_manager.tasks, 'ERROR: The task ID does not exist.\n'
+            detector = task_manager.tasks[task_id].detector
+            weight = detector._weight
+            device = detector._device
+            dnn = detector._dnn
+            half = detector._half
+            model_info = model_manager.get_model_info(weight, device, dnn, half)
+            model_state = model_info.get_state()
+            if model_state == mm.NOT_LOADED:
+                detector.load_model()
+            elif model_state == mm.LOADING:
+                response_message += 'Model is loading.\n'
+            elif model_state == mm.LOADING_COMPLETED:
+                response_message += 'Model loading completed.\n'
+            # TODO 简单实现，阻塞等待
+            while 1:
+                model_state = model_info.get_state()
+                if model_state == mm.LOADING_COMPLETED:
+                    break
+                time.sleep(1)
+        except Exception as e:
+            response_code = 400
+            response_message += traceback.format_exc()
+        response = service_coordinator_pb2.StartResponse()
         response.response.code = response_code
         response.response.message = response_message
         return response
