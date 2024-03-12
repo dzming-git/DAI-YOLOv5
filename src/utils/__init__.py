a235b355fd725dc96b0b4dffe61e8302ddccfa6b
dzming <dzm_work@163.com>
2024-03-12 11:02:34 +0000

    utils使用子模块进行复用

--- src/utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from .get_local_ip_address import get_local_ip_address
-from .singleton import singleton
--- src/utils/singleton.py
+++
@@ -1,7 +0,0 @@
-def singleton(cls):
-    instances = {}
-    def wrapper(*args, **kwargs):
-        if cls not in instances:
-            instances[cls] = cls(*args, **kwargs)
-        return instances[cls]
-    return wrapper
