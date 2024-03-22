310a3fbc6a35b840fea923bb267e628e20773953
dzming <dzm_work@163.com>
2024-03-22 05:32:02 +0000

    优化变量名

--- src/task_manager/task_manager.py
+++ src/task_manager/task_manager.py
@@ -1,64 +1,63 @@
 from src.utils import singleton
-from typing import Dict, Tuple, List
+from typing import Dict, List
 import queue
 from src.grpc.clients.image_harmony.image_harmony_client import ImageHarmonyClient
 from src.wrapper.yolov5_detector import YOLOv5Detector
-import traceback
 import threading
 
 class TaskInfo:
     def __init__(self, task_id: int):
         self.id: int = task_id
         
-        self.image_harmony_address: List[str, str] = []
-        self.image_harmony_client: ImageHarmonyClient = None
+        self.__image_harmony_address: List[str, str] = []
+        self.__image_harmony_client: ImageHarmonyClient = None
         
-        self.loader_args_hash: int = 0  # image harmony中加载器的hash值
+        self.__loader_args_hash: int = 0  # image harmony中加载器的hash值
         self.weight: str = ''
-        self.device_str: str = ''
+        self.__device_str: str = ''
         self.image_id_queue: queue.Queue[int] = queue.Queue()
         self.detector: YOLOv5Detector = None
-        self.stop_event = threading.Event()
-        self.track_thread = None  # 用于跟踪线程的引用
+        self.__stop_event = threading.Event()
+        self.__track_thread = None  # 用于跟踪线程的引用
     
     def set_pre_service(self, pre_service_name: str, pre_service_ip: str, pre_service_port: str, args: Dict[str, str] = {}):
         if 'image harmony gRPC' == pre_service_name:
-            self.image_harmony_address = [pre_service_ip, pre_service_port]
-            self.image_harmony_client = ImageHarmonyClient(pre_service_ip, pre_service_port)
+            self.__image_harmony_address = [pre_service_ip, pre_service_port]
+            self.__image_harmony_client = ImageHarmonyClient(pre_service_ip, pre_service_port)
             if 'LoaderArgsHash' not in args:
                 raise ValueError('Argument "LoaderArgsHash" is required but not set.')
-            self.loader_args_hash = int(args['LoaderArgsHash'])
+            self.__loader_args_hash = int(args['LoaderArgsHash'])
     
     def set_cur_service(self, args: Dict[str, str]):
         # TODO 未来添加dnn half等
         if 'Device' in args:
-            self.device_str = args['Device']
+            self.__device_str = args['Device']
         if 'Weight' in args:
             self.weight = args['Weight']
     
     def check(self) -> None:
-        if not self.image_harmony_address:
+        if not self.__image_harmony_address:
             raise ValueError('Error: image_harmony_address not set.')
-        if not self.image_harmony_client:
+        if not self.__image_harmony_client:
             raise ValueError('Error: image_harmony_client not set.')
-        if not self.loader_args_hash:
+        if not self.__loader_args_hash:
             raise ValueError('Error: loader_args_hash not set.')
         if not self.weight:
             raise ValueError('Error: weight not set.')
-        if not self.device_str:
+        if not self.__device_str:
             raise ValueError('Error: device not set.')
 
     def start(self):
         self.check()
-        self.image_harmony_client.connect_image_loader(self.loader_args_hash)
+        self.__image_harmony_client.connect_image_loader(self.__loader_args_hash)
         yolov5_builder = YOLOv5Detector.YOLOv5Builder()
         yolov5_builder.weight = self.weight
-        yolov5_builder.device_str = self.device_str
+        yolov5_builder.device_str = self.__device_str
         self.detector = yolov5_builder.build()
         self.detector.load_model()
-        self.stop_event.clear()  # 确保开始时事件是清除状态
-        self.track_thread = threading.Thread(target=self.detect_by_image_id)
-        self.track_thread.start()
+        self.__stop_event.clear()  # 确保开始时事件是清除状态
+        self.__track_thread = threading.Thread(target=self.detect_by_image_id)
+        self.__track_thread.start()
 
     # def progress(self):
     #     # TODO 两种方案，延迟？性能？目前采用的是延迟最低的方案
@@ -82,8 +81,8 @@ class TaskInfo:
     
     def detect_by_image_id(self):
         assert self.detector, 'yolov5 detector is not set\n'
-        assert self.image_harmony_client, 'image harmony client is not set\n'
-        while not self.stop_event.is_set():  # 使用事件来检查停止条件
+        assert self.__image_harmony_client, 'image harmony client is not set\n'
+        while not self.__stop_event.is_set():  # 使用事件来检查停止条件
             # image_id_in_queue = self.image_id_queue.get()
             try:
             # 尝试从队列中获取image_id，设置超时时间为1秒
@@ -91,11 +90,11 @@ class TaskInfo:
             except queue.Empty:
                 # 如果在超时时间内没有获取到新的image_id，则继续循环，此时可以检查停止事件
                 continue
-            width, height = self.image_harmony_client.get_image_size_by_image_id(image_id_in_queue)
+            width, height = self.__image_harmony_client.get_image_size_by_image_id(image_id_in_queue)
             if 0 == width or 0 == height:
                 continue
             new_unpad_width, new_unpad_height, top, bottom, left, right = self.detector.get_letterbox_size(width, height)
-            image_id, image = self.image_harmony_client.get_image_by_image_id(image_id_in_queue, new_unpad_width, new_unpad_height)
+            image_id, image = self.__image_harmony_client.get_image_by_image_id(image_id_in_queue, new_unpad_width, new_unpad_height)
             if 0 == image_id:
                 continue
             if not self.detector.add_image(image_id, image):
@@ -103,10 +102,10 @@ class TaskInfo:
             self.detector.detect_by_image_id(image_id)
     
     def stop(self):
-        self.stop_event.set()  # 设置事件，通知线程停止
-        if self.track_thread:
-            self.track_thread.join()  # 等待线程结束
-        self.image_harmony_client.disconnect_image_loader()
+        self.__stop_event.set()  # 设置事件，通知线程停止
+        if self.__track_thread:
+            self.__track_thread.join()  # 等待线程结束
+        self.__image_harmony_client.disconnect_image_loader()
         if self.detector:
             del self.detector  # 释放资源
         self.detector = None                                                                                                                                    
